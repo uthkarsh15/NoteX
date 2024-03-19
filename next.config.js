0b6e2e92e2bf3e0bc0e9a817d68f2d764c13cb90
Uthkarsh Gupta <shivamg1505@gmail.com>
2024-03-19 19:37:29 +0530

    edge function

--- next.config.js
+++ next.config.js
@@ -1,15 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ["firebasestorage.googleapis.com"],
-    },
-    typescript: {
-      ignoreBuildErrors: true,
-    },
-    eslint: {
-      ignoreDuringBuilds: true,
-    },
-  };
-  
-  module.exports = nextConfig;
-  
+  images: {
+    domains: ["firebasestorage.googleapis.com"],
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
+
+module.exports = nextConfig;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,7 +7,7 @@ import Provider from "@/components/Provider";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Notex",
+  title: "NoteX",
 };
 
 export default function RootLayout({
